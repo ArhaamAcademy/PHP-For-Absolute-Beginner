ec2b1de0df71600faa8a5b08d5dbc727863517d9
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2021-06-01 08:11:42 +0600

    Add files via upload

---
+++ inc/sidebar.php
@@ -0,0 +1,217 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+	<div class="sidebar-sticky">
+		<ul class="nav flex-column">
+			<li class="nav-item">
+				<a class="nav-link active" href="index.php">
+					<span data-feather="home"></span>
+					Home <span class="sr-only">(current)</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="index.php?lesson=php-syntax">
+					<span data-feather="file"></span>
+					PHP Syntax
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Comments
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Variables
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Numbers
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Strings
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Arrays
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Conditionals
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Loops
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Functions
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Dates
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Including files
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Working with File System
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					OOP
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					cURL
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Users CRUD (bad version)
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Database connection
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Select & display users from MySQL
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Creating Users functionality
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					$_GET & $_POST
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Save data in MySQL
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Form Validation
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Uploading Image
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Delete Users
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Update Users
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Users Search
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					CRUD: Code refactoring
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Namespaces & Autoloading with composer
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Install packages using composer
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					MVC Framework with Custom Routing
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					MVC Controllers
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					MVC Views
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					MVC Models
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Setup Apache Virtual Hosts
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Sessions
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Cookies
+				</a>
+				<a class="nav-link" href="#">
+					<span data-feather="file"></span>
+					Others
+				</a>
+			</li>
+			<!--
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="shopping-cart"></span>
+                  Products
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Customers
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="bar-chart-2"></span>
+                  Reports
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="layers"></span>
+                  Integrations
+                </a>
+              </li>
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Saved reports</span>
+              <a class="d-flex align-items-center text-muted" href="#">
+                <span data-feather="plus-circle"></span>
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Current month
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Last quarter
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Social engagement
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Year-end sale
+                </a>
+              </li>
+-->
+		</ul>
+	</div>
+</nav>
